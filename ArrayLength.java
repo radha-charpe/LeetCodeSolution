9991156eda8fb18d43ce82dbffbc4f8a88fee25e
radha-charpe <radhacharpe016@gmail.com>
2025-08-27 01:18:45 +0530

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,9 @@
+package com.arr;
+
+public class ArrayLength {
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5 };
+		System.out.println("length of array is :" + arr.length);
+	}
+
+}
